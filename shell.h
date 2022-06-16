7daf98b3ee7af077d54fcc7dbb13b38b286d66d9
bazboy36 <shokunbiabdulbasit1@gmail.com>
2022-06-15 19:01:29 -0700

    updatedall files agian..hurray

--- shell.h
+++ shell.h
@@ -1,71 +1,79 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
+/* STD LIBRARIES */
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
 #include <errno.h>
+#include <stddef.h>
+#include <unistd.h>
 #include <signal.h>
 
+/*---------------------------------------------------------*/
+
+/* MACROS */
+#define SIGINT 2
+extern char **environ;
+extern int errno;
+
+/*---------------------------------------------------------*/
+
+/* PROTOTYPES */
+
+/*-----Main functions-----*/
+int shell_interactive(void);
+int shell_no_interactive(int ac, char **av);
+
+/*-----String functions-----*/
+int _strlen(char *string);
+int _strcmp(char *s1, char *s2);
+char *_strncmp(char *PATH, char *MATCH, size_t n);
+int count_words(char *str);
+char *_strdup(char *str);
+char *_strcat(char *dest, char *src);
+char *_itoa(int i, char *strout, int base);
+
+/*-----Aux functions-----*/
+char **tokenizer(char *s, char *delim);
+int (*get_builtin(char *string))();
+int fork_process(char *path, char **tokens, char **environ);
+char *find_path(char **tokens);
+char *prompt_no_interactive(char *name_file);
+char *prompt_interactive(char *name_file);
+void handle_sigint(int sig);
+void free_all(char **tokens);
+
+/*-----Error_messages-----*/
+void path_error(int numerr, char **tokens, char *name_file);
+void permission_error(int numerr, char **tokens, char *name_file);
+
+/*-----Built-ins-----*/
+int _own_exit(void);
+int env(void);
+
+/*---------------------------------------------------------*/
+
+/*STRUCTURES*/
+
 /**
- * struct list - path directory list structure.
- * @dir: directory path.
- * @next: pointer to next directory node.
- */
-typedef struct list
+*struct builtin - builtin structures.
+*@name: a char with a name of a file.
+*@func: a int that call.
+*/
+
+typedef struct builtin
 {
-	char *dir;
-	struct list *next;
-} list_t;
+	char *name;
+	int (*func)();
+} built_in;
 
-/* main.c */
-int start_shell(list_t *path, char **env, char *program_name, int mode);
-int execute_buffer(char *buffer, list_t *path, char **env, char *program_name);
-int execute_command(char *new_buffer, list_t *path, char **env,
-			int final, char *program_name);
-int execute_fork(char **input, char *program_name);
+static const built_in list[] = {
+	{"env", env},
+	{"exit", _own_exit},
+	{NULL, NULL}};
 
-/* env-list.c */
-list_t *list_path(char **env);
-list_t *create_list(char **environ);
-list_t *add_list(list_t **head, char *dir);
-void free_list(list_t *head);
 
-/* dmemory.c */
-char *get_path(char *buffer, list_t **path);
-char *aux_get_path(list_t *list_pointer,
-	char *slash_command, char *slash_input, char *input);
-char *clean_spaces(char *buffer);
-char **create_argv(char *input_buffer, list_t **path);
-void free_argv(char **argv);
-
-/* built-ins.c */
-char *clean_comments(char *buffer);
-int check_builtin(char *command);
-int check_syntax(char *buffer);
-int builtins(char **input, char **env);
-void print_help(char **input);
-
-/* strings-1.c */
-int str_len(char *s);
-char *str_cpy(char *dest, char *src);
-char *str_dup(char *str);
-char *str_cat(char *dest, char *src);
-char *str_con(char *s1, char *s2);
-
-/* strings-2.c */
-int not_empty(char *input_buffer);
-int str_twins(char *s1, char *s2);
-int str_count(char *buffer, char c);
-char *str_tr(char *buffer, char old_char, char new_char);
-
-/* errors.c */
-void ctrl_c(__attribute__((unused)) int x);
-void print_error(char *program_name, char *input, int error_num);
-
-#endif
+#endif /* SHELL.H */
